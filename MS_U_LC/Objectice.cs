41a30328dcb0007b20a48ebe406365cea2477331
Mateusz <mateusz@socialcee.com>
2018-10-24 14:40:36 +0200

    init

---
+++ MS_U_LC/Objectice.cs
@@ -0,0 +1,10 @@
+/*
+
+Stworzenie małej aplikacji, która dla zadanych wartości wyliczy stałe, miesięczne raty kredytu.
+Parametry wejściowe formularza:
+Kwota kredytu, Roczne oprocentowanie, Okres kredytowania w miesiącach.
+
+Wyniki powinny być przedstawione w formie tabeli i zawierać następujące informacje:
+Numer miesiąca, wysokość raty (część kapitałowa, część odsetkowa), pozostało do spłaty (kapitał, odsetki)
+
+*/
---
+++ MS_U_LC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoanCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
