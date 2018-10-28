a0c0211f908a88a0afca7eabe8159a21e8fc6114
Mati ZejnoV <mati.szwaba@gmail.com>
2018-10-28 22:07:21 +0100

    input

---
+++ MS_U_LC/LoanCalculator_WPF/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using LoanCalculator.Model;
+
+namespace LoanCalculator_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var inputData = new InputData()
+            {
+                LoanAmount = decimal.Parse(AmountTxt.Text),
+                AnnualInterestRate = double.Parse(PercentageTxt.Text),
+                LoanTerm = int.Parse(TermTxt.Text)
+            };
+        }
+    }
+}
